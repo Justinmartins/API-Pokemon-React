9876566fcb18c2bb2ed9d3f25a68bc917bc8ccbb
Justin Martins <justin.martins95@gmail.com>
2025-01-29 15:16:12 +0100

    Update style

--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -7,7 +7,7 @@ export function Input({ value, placeholder, onChange }) {
       value={value}
       placeholder={placeholder}
       onChange={(e) => onChange(e.target.value)}
-      className="w-full p-2 border border-gray-300 rounded-lg"
+      className="w-full p-2 border border-gray-300 rounded-lg bg-white"
     />
   );
 }
